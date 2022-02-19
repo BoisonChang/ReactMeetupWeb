4f6daf71a75e43d7550e5855c94e4d5792b022b9
BoisonChang <dragoncres@gmail.com>
2022-02-19 14:48:26 +0800

    fix homepage

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div>
       <Layout>
         <Routes>
-            <Route path='/' element={<AllMeetupsPage/>}></Route>
+            <Route path={'' || '/'} element={<AllMeetupsPage/>}></Route>
             <Route path='/new-meetup' element={<NewMeetupPage/>}></Route>
             <Route path='/favorites' element={<FavoritesPage/>}></Route>
           </Routes>
